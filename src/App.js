0f11c844161b5014adacf9c1857cbeee235b6140
Ashmit Tyagi <atyagi@infrablok.com>
2023-06-22 13:51:39 +0530

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import { Fragment, useState } from "react";
 import Home from "./screens/home/Home";
 import About from "./screens/about/About";
-import { Button, Container, Grid, Typography } from "@material-ui/core";
+import { Button, Container, Grid } from "@material-ui/core";
 import Login from "./screens/login/Login";
 
 const App = () =>{
-   const[currentPage,setCurrentPage]=useState("home");
+   const[currentPage,setCurrentPage]=useState("login");
+   const[isLoggedIn,setIsLoggedin]=useState(false);
+   const[email,setEmail]=useState('');
+   const[password,setPassword]=useState('')
+
+
+   
    const handlePageChange = (page) => {
     setCurrentPage(page);
    };
 
+   const handleLogin = () =>{
+    if(email==='hi@gmail.com' && password==='password'){
+      setIsLoggedin(true);
+      setCurrentPage('home');
+    }
+    else{
+      alert('Invalid email or password')
+    }
+   }
+
    const renderPage = () =>{
     switch (currentPage) {
+      case'login':
+      return (
+        <Login 
+        email={email}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleLogin={handleLogin}
+       />
+      );
       case 'home':
         return <Home />;
       case 'about':
         return <About />;
-      case 'login':
-        return <Login />;
+     
       default:
         return null;
     }
@@ -28,15 +53,19 @@ const App = () =>{
           <Fragment>
           <Grid container direction="column" alignItems="center" spacing={2}>
         <Grid item>
+        
+            <>
           <Button onClick={() => handlePageChange('home')}>Home</Button>
           <Button onClick={() => handlePageChange('about')}>About</Button>
           <Button onClick={() => handlePageChange('login')}>Login</Button>
+          </>
+      
           </Grid>
         <Grid item>
-          <Typography
+          {/* <Typography
            variant="h4" align="center">
             Welcome to {currentPage} page
-          </Typography>
+          </Typography> */}
         </Grid>
         <Grid item>{renderPage()}</Grid>
         </Grid>
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,17 +1,29 @@
-import { Typography } from "@material-ui/core";
-import { Fragment } from "react";
+import React from 'react';
+import { TextField, Button } from '@mui/material';
 
-
-
-const Login = () => {
-    return (
-        <Fragment>
-             <Typography variant="body1" align="center">
-                Login Page Details
-            </Typography>
-        </Fragment>
-
-    )
-}
+const Login = ({ email, password, setEmail, setPassword, handleLogin }) => {
+  return (
+    <>
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        fullWidth
+        margin="normal"
+      />
+      <Button onClick={handleLogin} variant="contained" color="primary">
+        Login
+      </Button>
+    </>
+  );
+};
 
 export default Login;
