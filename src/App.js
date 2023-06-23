d89893ebdc4c18cd409d7751692148bf64661780
Ashmit Tyagi <atyagi@infrablok.com>
2023-06-23 18:05:49 +0530

    Code Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { Fragment, useState } from "react";
-import Home from "./screens/home/Home";
-import About from "./screens/about/About";
-import { Button, Container, Grid } from "@material-ui/core";
-import Login from "./screens/login/Login";
-import { Typography } from "@mui/material";
-import { userInfo } from "./dummydata/dummyData";
+
 import { Routes, Route  } from "react-router";
 import NotFound from "./notfound/NotFound";
 import Data from "./screens/data/Data";
