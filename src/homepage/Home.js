169c8d11d8afd81ff6d21bd2e2a0b3b372b411ed
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-03 17:36:08 +0530

    .

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -14,7 +14,8 @@ const Home = () => {
   const fetchData = async () => {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-      const data = response.json();
+      const data = await response.json();
+      console.log(data);
       setUsers(data);
     } catch (error) {
       console.log('Error fetching data:', error);
