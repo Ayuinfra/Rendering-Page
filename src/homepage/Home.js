f9d9e886a7854e05831db9047b3f6b9d5306d5bf
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-03 11:25:46 +0530

    Code Updated

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { AddCircleOutline, Edit, Delete } from '@mui/icons-material';
-import { Button, Container, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 
 const Home = ({ users, handleAddUser, handleEditUser, handleDeleteUser }) => {
+  const [deleteUserId, setDeleteUserId] = useState(null);
+
+  const handleDeleteConfirmation = (userId) => {
+    setDeleteUserId(userId);
+  };
+
+  const handleDeleteCancel = () => {
+    setDeleteUserId(null);
+  };
+
+  const handleDeleteConfirm = () => {
+    handleDeleteUser(deleteUserId);
+    setDeleteUserId(null);
+  };
+
   const sortedUsers = users.slice().sort((a, b) => {
     const nameA = `${a.firstName} ${a.lastName}`.toLowerCase();
     const nameB = `${b.firstName} ${b.lastName}`.toLowerCase();
@@ -12,22 +27,22 @@ const Home = ({ users, handleAddUser, handleEditUser, handleDeleteUser }) => {
 
   return (
     <Container  >
-<>
+      <>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>
-              <Typography variant="subtitle1" >
+              <Typography variant="subtitle1" fontWeight="bold">
                 <b>First Name</b>
               </Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="subtitle1" >
+              <Typography variant="subtitle1" fontWeight="bold">
               <b>Last Name</b>
               </Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="subtitle1" >
+              <Typography variant="subtitle1" fontWeight="bold">
               <b>Email</b>
               </Typography>
             </TableCell>
@@ -60,7 +75,7 @@ const Home = ({ users, handleAddUser, handleEditUser, handleDeleteUser }) => {
                   <IconButton onClick={() => handleEditUser(user.id)}>
                     <Edit />
                   </IconButton>
-                  <IconButton onClick={() => handleDeleteUser(user.id)}>
+                  <IconButton onClick={() => handleDeleteConfirmation(user.id)}>
                     <Delete />
                   </IconButton>
                 </TableCell>
@@ -69,6 +84,21 @@ const Home = ({ users, handleAddUser, handleEditUser, handleDeleteUser }) => {
           )}
         </TableBody>
       </Table>
+
+      <Dialog open={deleteUserId !== null} onClose={handleDeleteCancel}>
+        <DialogTitle>Confirmation</DialogTitle>
+        <DialogContent>
+          <Typography>Are you sure you want to delete?</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteCancel} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteConfirm} color="primary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
     </Container>
     
