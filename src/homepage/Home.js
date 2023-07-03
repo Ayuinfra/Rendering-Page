fdc2b31245f2eb1555f80a96f1f175b9cab1f632
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-03 17:39:55 +0530

    .

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -7,6 +7,7 @@ import React, { useState, useEffect } from 'react';
 const Home = ({ users, handleAddUser, handleEditUser, handleDeleteUser }) => {
   const [note, setNote] = useState([]);
 
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -21,7 +22,21 @@ const Home = ({ users, handleAddUser, handleEditUser, handleDeleteUser }) => {
       console.log('Error fetching data:', error);
     }
   };
-  
+  const [deleteUserId, setDeleteUserId] = useState(null);
+
+  const handleDeleteConfirmation = (userId) => {
+    setDeleteUserId(userId);
+  };
+
+  const handleDeleteCancel = () => {
+    setDeleteUserId(null);
+  };
+
+  const handleDeleteConfirm = () => {
+    handleDeleteUser(deleteUserId);
+    setDeleteUserId(null);
+  };
+
 
   return (
     <Container maxWidth = "md" sx={{marginTop:'2rem',display:'flexstart'}}>
