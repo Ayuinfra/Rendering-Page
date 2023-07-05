3b949626fa0f06ffcb7d5d1ab1561ea91c85ae8f
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-05 14:47:08 +0530

    MarginBottom

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -1,9 +1,9 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
+import { IconButton, InputAdornment, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 import { Container } from '@mui/system';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Edit } from '@mui/icons-material';
-import { Delete, Search } from '@mui/icons-material';
+import { Delete, Search, Clear } from '@mui/icons-material';
 
 const Home = () => {
   const [note, setNote] = useState([]);
@@ -24,7 +24,7 @@ const Home = () => {
 
   const getRowColor = (completed) => {
     if (completed) {
-      return 'green';
+      return 'YellowGreen';
     } else {
       return 'orange';
     }
@@ -34,23 +34,38 @@ const Home = () => {
     setSearchTerm(event.target.value);
   };
 
+  const clearSearch = () => {
+    setSearchTerm('');
+  };
+
   // Filter and sort the data based on the search term and task title
   const filteredAndSortedNote = note
     .filter((user) => user.title.toLowerCase().includes(searchTerm.toLowerCase()))
     .sort((a, b) => a.title.localeCompare(b.title));
 
   return (
-    <Container maxWidth="md" sx={{ marginTop: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <Container maxWidth="md" sx={{ marginTop: '3rem', display: 'flex', flexDirection: 'column', alignItems: 'center' , marginBottom:'3rem' }}>
       <TextField
         label="Search"
         variant="outlined"
         value={searchTerm}
         onChange={handleSearch}
         InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <IconButton size="small" edge="start" aria-label="search">
+                <Search />
+              </IconButton>
+            </InputAdornment>
+          ),
           endAdornment: (
-            <IconButton size="small" edge="end" aria-label="search">
-              <Search />
-            </IconButton>
+            <InputAdornment position="end">
+              {searchTerm && (
+                <IconButton size="small" edge="end" aria-label="clear" onClick={clearSearch}>
+                  <Clear />
+                </IconButton>
+              )}
+            </InputAdornment>
           ),
           sx: { borderRadius: '50px' },
         }}
@@ -59,7 +74,7 @@ const Home = () => {
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
-            <TableRow style={{ backgroundColor: 'CadetBlue' }}>
+            <TableRow style={{ backgroundColor: 'Cadetblue' }}>
               <TableCell style={{ color: 'Black' }}><b>Task</b></TableCell>
               <TableCell style={{ color: 'Black' }}><b>Body</b></TableCell>
               <TableCell style={{ color: 'Black' }}><b>Status</b></TableCell>
