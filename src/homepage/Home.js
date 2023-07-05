aabbb11a3ea922fded2c63599bfd54e9fb83700b
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-05 15:43:11 +0530

    Pagination

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -2,12 +2,16 @@ import { IconButton, Paper, TextField } from '@mui/material';
 import { Container } from '@mui/system';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Clear  } from '@mui/icons-material';
-import { Search } from '@mui/icons-material';
+import { Clear } from '@mui/icons-material';
+import {  Search } from '@mui/icons-material';
+import Pagination from '@mui/material/Pagination';
 
 const Home = () => {
   const [note, setNote] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 6;
+  const totalPages = Math.ceil(note.length / itemsPerPage);
 
   useEffect(() => {
     fetchData();
@@ -15,19 +19,26 @@ const Home = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/photos')
+      const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
       setNote(response.data);
     } catch (error) {
       // Handle error
     }
   };
 
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+    setPage(1); // Reset the page number when a new search term is entered
   };
 
   const clearSearch = () => {
     setSearchTerm('');
+    setPage(1); // Reset the page number when the search term is cleared
+  };
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
   };
 
   // Filter and sort the data based on the search term and task title
@@ -35,8 +46,16 @@ const Home = () => {
     .filter((user) => user.title.toLowerCase().includes(searchTerm.toLowerCase()))
     .sort((a, b) => a.title.localeCompare(b.title));
 
+  // Calculate the index range of items to display for the current page
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Get the current page of items
+  const paginatedNote = filteredAndSortedNote.slice(startIndex, endIndex);
+
   return (
-    <Container maxWidth="md" sx={{ marginTop: '3rem', display: 'flex', flexDirection: 'column', alignItems: 'center' , marginBottom: '3rem'}}>
+    <Container maxWidth="md" sx={{ marginTop: '3rem', display: 'flex', flexDirection: 'column', alignItems: 'center' , marginBottom:'2rem'}}>
+      
       <TextField
         label="Search"
         variant="outlined"
@@ -58,13 +77,30 @@ const Home = () => {
         sx={{ marginBottom: '1rem', width: '300px' }}
       />
       <Container sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '1rem' }}>
-        {filteredAndSortedNote.map((user) => (
-          <Paper key={user.id} sx={{ padding: '1rem', textAlign: 'center' }}>
-            <img src={user.thumbnailUrl} alt={user.title} style={{ width: '100%', cursor: 'pointer' }} onClick={() => window.open(user.url, '_blank')} />
+        {paginatedNote.map((user) => (
+          <Paper
+            key={user.id}
+            sx={{
+              padding: '1rem',
+              textAlign: 'center',
+            }}
+          >
+            <img
+              src={user.thumbnailUrl}
+              alt={user.title}
+              style={{ width: '100%', cursor: 'pointer' }}
+              onClick={() => window.open(user.url, '_blank')}
+            />
             <p>{user.title}</p>
           </Paper>
         ))}
       </Container>
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={handlePageChange}
+        sx={{ marginTop: '1rem' }}
+      />
     </Container>
   );
 };
