ca59fd9aee4aff1e68f3ea8413ee81b3961cd1f8
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-05 09:44:25 +0530

    Row Color and edit button Changes

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -1,4 +1,3 @@
-
 import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { Container } from '@mui/system';
 import React, { useState, useEffect } from 'react';
@@ -17,44 +16,51 @@ const Home = () => {
     try {
       const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
       setNote(response.data);
-      console.log(response.data)
     } catch (error) {
-     
+      
     }
   };
-  
+
+  const getRowColor = (completed) => {
+    if (completed) {
+      return 'YellowGreen';
+    } else {
+      return 'orange';
+    }
+  };
 
   return (
-    <Container maxWidth = "md" sx={{marginTop:'2rem',display:'flexstart',marginBottom:'3rem'}}>
-<TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell><b>Task</b></TableCell>
-            <TableCell><b>Status</b></TableCell>
-            <TableCell><b>Actions</b></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {note.map((users) => (
-            <TableRow key={users.id}>
-              <TableCell>{users.title}</TableCell>
-              <TableCell>{users.completed ? 'Completed' : 'Incomplete'}</TableCell>
-              <TableCell>
-                <IconButton variant="outlined" color="primary">
-                <Edit />
-                </IconButton>
-                <IconButton variant="outlined" color="secondary">
-                  <Delete />
-                </IconButton>
-              </TableCell>
+    <Container maxWidth="md" sx={{ marginTop: '3rem', display: 'flexstart', marginBottom: '3rem' }}>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><b>Task</b></TableCell>
+              <TableCell><b>Status</b></TableCell>
+              <TableCell><b>Actions</b></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {note.map((user) => (
+              <TableRow key={user.id} style={{ backgroundColor: getRowColor(user.completed) }}>
+                <TableCell style={{ color: 'white' }}>{user.title}</TableCell>
+                <TableCell style={{ color: 'white' }}>{user.completed ? 'Completed' : 'Incomplete'}</TableCell>
+                <TableCell>
+                  {!user.completed && (
+                    <IconButton variant="outlined" color="primary">
+                      <Edit />
+                    </IconButton>
+                  )}
+                  <IconButton variant="outlined" color="secondary">
+                    <Delete />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
-    
   );
 };
 
