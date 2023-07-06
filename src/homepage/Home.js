0e672b8c74fc9180240af82349ecf9ce08fdb61b
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-06 10:17:30 +0530

    Code Updated

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -2,14 +2,14 @@ import { IconButton, Paper, TextField } from '@mui/material';
 import { Container } from '@mui/system';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Clear, Search } from '@mui/icons-material';
+import { Clear } from '@mui/icons-material';
+import { Search } from '@mui/icons-material';
 import Pagination from '@mui/material/Pagination';
 
 const Home = () => {
   const [note, setNote] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [page, setPage] = useState(1);
-  const [openImage, setOpenImage] = useState(null); // Track the currently opened URL image
   const itemsPerPage = 6;
   const totalPages = Math.ceil(note.length / itemsPerPage);
 
@@ -26,6 +26,7 @@ const Home = () => {
     }
   };
 
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
     setPage(1); // Reset the page number when a new search term is entered
@@ -38,16 +39,6 @@ const Home = () => {
 
   const handlePageChange = (event, value) => {
     setPage(value);
-  };
-
-  const handleImageClick = (url) => {
-    if (openImage === url) {
-      // Close the URL image and show the home screen
-      setOpenImage(null);
-    } else {
-      // Open the URL image
-      setOpenImage(url);
-    }
   };
 
   // Filter and sort the data based on the search term and task title
@@ -63,7 +54,7 @@ const Home = () => {
   const paginatedNote = filteredAndSortedNote.slice(startIndex, endIndex);
 
   return (
-    <Container maxWidth="md" sx={{ marginTop: '3rem', display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '2rem' }}>
+    <Container maxWidth="md" sx={{ marginTop: '3rem', display: 'flex', flexDirection: 'column', alignItems: 'center' , marginBottom:'2rem'}}>
       <TextField
         label="Search"
         variant="outlined"
@@ -84,6 +75,12 @@ const Home = () => {
         }}
         sx={{ marginBottom: '1rem', width: '300px' }}
       />
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={handlePageChange}
+        sx={{ marginTop: '1rem' , marginBottom:'2rem'}}
+      />
       <Container sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '1rem' }}>
         {paginatedNote.map((user) => (
           <Paper
@@ -97,39 +94,12 @@ const Home = () => {
               src={user.thumbnailUrl}
               alt={user.title}
               style={{ width: '100%', cursor: 'pointer' }}
-              onClick={() => handleImageClick(user.url)}
+              onClick={() => window.open(user.url, '_blank')}
             />
             <p>{user.title}</p>
           </Paper>
         ))}
       </Container>
-      <Pagination
-        count={totalPages}
-        page={page}
-        onChange={handlePageChange}
-        sx={{ marginTop: '1rem' }}
-      />
-      {openImage && (
-        
-       <>
-       
-        <IconButton
-            size="small"
-            edge="end"
-            aria-label="close"
-            onClick={() => setOpenImage(null)}
-            style={{ position: 'fixed', top: '1rem', right: '1rem', color: '#fff' }}
-          >
-            <Clear />
-          </IconButton>
-          <img
-            src={openImage}
-            alt="Full Size"
-            style={{ maxWidth: '80%', maxHeight: '80%', position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}/>
-       </>  
-      
-      
-      )}
     </Container>
   );
 };
