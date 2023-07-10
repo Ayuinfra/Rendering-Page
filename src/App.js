d4ff633c149709d56f0a1a8c8f1452681dee473e
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-10 11:21:41 +0530

    Comments Added in App.js

--- src/App.js
+++ src/App.js
@@ -1,104 +1,102 @@
 import { Fragment, useCallback, useState } from "react";
 import CrudInnerApp from './crud/InnerApp';
 import Home from "./home/Home";
-import Images from "./images/InnerApp1"
+import Images from "./images/InnerApp1";
 import { Drawer, IconButton, List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import CubeCounter from "./cube/InnerApp2";
 
-
 export const Projects = {
-  CRUD : "crud",
-  IMAGES : "images",
-  CUBE : "cube",
-}
+  CRUD: "crud",
+  IMAGES: "images",
+  CUBE: "cube",
+};
 
-const projectsList = ["CRUD","IMAGES","CUBE"];
+const projectsList = ["CRUD", "IMAGES", "CUBE"];
 
-const App = ()=>{
+const App = () => {
+  //* State to track the current project
+  const [currentProject, setCurrentProject] = useState(); 
+ 
+  //* State to track the drawer open/close
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false); 
 
-  const [currentProject,setCurrentProject] = useState();
-  const [isDrawerOpen,setIsDrawerOpen] = useState(false);
 
-  const handleDrawerClose = ()=>{
+  //* Handler to close the drawer
+  const handleDrawerClose = () => {
     setIsDrawerOpen(false);
-  }
+  };
 
-  const handleDrawerOpen = ()=>{
+
+   //* Handler to open the drawer
+  const handleDrawerOpen = () => {
     setIsDrawerOpen(true);
-  }
+  };
 
-  const onSetCurrentProjectHandler = useCallback((projectName)=>{
-    setCurrentProject(projectName)
-  },[])
-  
+  const onSetCurrentProjectHandler = useCallback((projectName) => {
+    
+    //* Set the current project
+    setCurrentProject(projectName);
 
-  const onChangeProjectHandler = (projectName)=>{
+    // Close the drawer after setting the project
+    setIsDrawerOpen(false); 
+  }, []);
+
+  const onChangeProjectHandler = (projectName) => {
     if (!projectName) {
+
+      // If no project selected, show the Home component
       return <Home onSetCurrentProject={onSetCurrentProjectHandler} />;
     }
 
-    switch(projectName){
-      case "CRUD" :
-        return(
-          <CrudInnerApp/>
-        
-      )
-      case "IMAGES" : 
-      return (
-        <Images/>
-      )
-      case "CUBE" : 
-      return (
-        <CubeCounter/>
-      )
-      default :
-        return(
-            <Home onSetCurrentProject={onSetCurrentProjectHandler}/>
-        )
-    }
-  }
-  
-  return(
-    <Fragment>
-        {onChangeProjectHandler(currentProject)}
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={handleDrawerOpen}
-          sx={{ position: 'absolute', left: '1rem', top: '1rem' }}
-        >
-          <MenuIcon />
-        </IconButton>
+    switch (projectName) {
+      // Render the CRUD component
+      case "CRUD":
+        return <CrudInnerApp />;
 
-        <Drawer
-          anchor="left"
-          open={isDrawerOpen}
-          onClose={handleDrawerClose}
-        >
+      // Render the Images component
+      case "IMAGES":
+        return <Images />;
+
+      // Render the CubeCounter component
+      case "CUBE":
+        return <CubeCounter />;
+        
+      default:
+        return <Home onSetCurrentProject={onSetCurrentProjectHandler} />;
+    }
+  };
+
+  return (
+    <Fragment>
+      {onChangeProjectHandler(currentProject)}
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={handleDrawerOpen}
+        sx={{ position: 'absolute', left: '1rem', top: '1rem' }}
+      >
+        <MenuIcon />
+      </IconButton>
+
+      <Drawer anchor="left" open={isDrawerOpen} onClose={handleDrawerClose}>
         <List>
-          <ListItem
-            key="home"
-            onClick={() => onSetCurrentProjectHandler(null)}
-          >
+          <ListItem key="home" onClick={() => onSetCurrentProjectHandler(null)}>
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
           </ListItem>
-          {projectsList.map((item,index) => (
-            <ListItem
-              key={index}
-              onClick={() => onSetCurrentProjectHandler(item)}
-            >
+          {projectsList.map((item, index) => (
+            <ListItem key={index} onClick={() => onSetCurrentProjectHandler(item)}>
               <ListItemText primary={item} />
             </ListItem>
           ))}
         </List>
       </Drawer>
     </Fragment>
+  );
+};
 
-  )
-}
 export default App;
