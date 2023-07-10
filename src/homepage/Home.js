e3a8a11aeec803f4ed569b5b57528e2a88f636d3
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-10 12:57:02 +0530

    data Fetch from the api

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -1,68 +1,72 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-import { Container } from '@mui/system';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
 import axios from 'axios';
-import { Edit } from '@mui/icons-material';
-import { Delete } from '@mui/icons-material';
 
 const Home = () => {
-  const [note, setNote] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
-    fetchData();
+    axios.get('https://fakestoreapi.com/products')
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error));
   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-      setNote(response.data);
-    } catch (error) {
-      
-    }
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
   };
 
-  const getRowColor = (completed) => {
-    if (completed) {
-      return 'YellowGreen';
-    } else {
-      return 'orange';
-    }
+  const handleBackClick = () => {
+    setSelectedProduct(null);
   };
-
-  
-  const sortedNote = [...note].sort((a, b) => a.title.localeCompare(b.title));
 
   return (
-    <Container maxWidth="md" sx={{ marginTop: '3rem', display: 'flexstart', marginBottom: '3rem' }}>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow style={{ backgroundColor: 'CadetBlue' }}>
-              <TableCell style={{ color: 'black' }}><b>Task</b></TableCell>
-              <TableCell style={{ color: 'black' }}><b>Status</b></TableCell>
-              <TableCell style={{ color: 'black' }}><b>Actions</b></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {sortedNote.map((user) => (
-              <TableRow key={user.id} style={{ backgroundColor: getRowColor(user.completed) }}>
-                <TableCell style={{ color: 'white' }}>{user.title}</TableCell>
-                <TableCell style={{ color: 'white' }}>{user.completed ? 'Completed' : 'Incomplete'}</TableCell>
-                <TableCell>
-                  {!user.completed && (
-                    <IconButton variant="outlined" color="primary">
-                      <Edit />
-                    </IconButton>
-                  )}
-                  <IconButton variant="outlined" color="secondary">
-                    <Delete />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+    <Container maxWidth="md" sx={{marginTop:'2rem' ,display : "flexStart"}}>
+      {!selectedProduct ? (
+        <Grid container spacing={3}>
+          {products.map(product => (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <Card onClick={() => handleProductClick(product)}>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={product.image}
+                  alt={product.title}
+                />
+                <CardContent>
+                  <Typography variant="h6" component="div">
+                    {product.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.price}$
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Card>
+          <CardMedia
+            component="img"
+            height="300"
+            image={selectedProduct.image}
+            alt={selectedProduct.title}
+          />
+          <CardContent>
+            <Typography variant="h6" component="div">
+              {selectedProduct.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {selectedProduct.description}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {selectedProduct.price}$
+            </Typography>
+            <button onClick={handleBackClick}>Back</button>
+          </CardContent>
+        </Card>
+      )}
     </Container>
   );
 };
