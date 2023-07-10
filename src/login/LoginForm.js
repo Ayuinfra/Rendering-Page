b743060c3e9c80b0752df8d5fe84cb13413c54a5
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-10 17:35:54 +0530

    Login Form Update

--- src/login/LoginForm.js
+++ src/login/LoginForm.js
@@ -9,44 +9,59 @@ const LoginForm = ({
   handlePasswordChange,
   handleLogin
 }) => {
-
   const navigate = useNavigate();
-  const onMoveSignupHandler = ()=>{
-      navigate('../signup')
-  }
 
-  return(
-  <Container maxWidth="sm">
-    <Card>
-      <CardContent>
-        <Typography variant="h5" component="div" align="center">
-          Login
-        </Typography>
-        <TextField
-          fullWidth
-          label="Email"
-          value={email}
-          onChange={handleEmailChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Password"
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-          margin="normal"
-        />
-        <Button variant="contained" onClick={handleLogin}>
-          Login
-        </Button>
-        <Button variant="contained" sx={{marginLeft : '10px'}} onClick={onMoveSignupHandler}>
-          Signup
-        </Button>
-      </CardContent>
-    </Card>
-  </Container>
-);
-}
+  const onMoveSignupHandler = () => {
+    navigate('../signup');
+  };
+
+  const validateLoginFields = () => {
+    if (email.trim() === '' || password.trim() === '') {
+      alert('Please enter email and password');
+      return false;
+    }
+    return true;
+  };
+
+  const handleLoginClick = () => {
+    if (validateLoginFields()) {
+      handleLogin();
+      navigate('/home');
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="div" align="center">
+            Login
+          </Typography>
+          <TextField
+            fullWidth
+            label="Email"
+            value={email}
+            onChange={handleEmailChange}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            margin="normal"
+          />
+          <Button variant="contained" onClick={handleLoginClick}>
+            Login
+          </Button>
+          <Button variant="contained" sx={{ marginLeft: '10px' }} onClick={onMoveSignupHandler}>
+            Signup
+          </Button>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
 
 export default LoginForm;
