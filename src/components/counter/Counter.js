2c968b0636fdf735c0adb5ff5b33c3c9887acbcd
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-17 11:19:21 +0530

    Buttons in counter updated

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,34 +1,45 @@
 import React from 'react';
-import { Button, Paper, Box } from '@mui/material';
-import { Container } from '@mui/system';
-import { decrement, increment } from '../../store/actions/Actions';
+import { Box, Button, Container, Paper } from '@mui/material';
 import { connect } from 'react-redux';
+import { increment, decrement } from '../../store/actions/Actions';
 
-const Counter = ({ handleIncrement, handleDecrement, handleIncrementBy2, handleDecrementBy2 }) => {
+const Counter = ({ handleIncrement, handleDecrement, handleIncrementByTwo, handleDecrementByTwo }) => {
   return (
-    <Container maxWidth="md" sx={{marginTop:"2rem",display :"flexStart"}}>
-    <Paper elevation={0}>
+    <Container maxWidth="md" sx={{marginTop:"1rem",display :"flexStart"}}>
+ <Paper elevation={0}>
       <Box marginBottom={1}>
-        <Button variant="contained" color="primary" onClick={handleIncrement}>
-          Add
-        </Button>
-        <Button variant="contained" color="secondary" onClick={handleDecrement}>
-          Subtract
-        </Button>
+      <Button variant="contained" color="primary" onClick={handleIncrement}>
+        Add
+      </Button>
+      <Button variant="contained" color="secondary" onClick={handleDecrement}   style={{ marginLeft: '10px' }}>
+        Sub
+      </Button>
       </Box>
       <Box marginBottom={1}>
-        <Button variant="contained" color="primary" onClick={handleIncrementBy2}>
-          Add + 2
-        </Button>
-        <Button variant="contained" color="secondary" onClick={handleDecrementBy2}>
-          Subtract - 2
-        </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleIncrementByTwo}
+        
+      >
+        Add + 2
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleDecrementByTwo}
+        style={{ marginLeft: '10px' }}
+      >
+        Sub - 2
+      </Button>
       </Box>
+     
     </Paper>
     </Container>
-
+   
   );
 };
+
 const mapDispatchToProps = (dispatch) => {
   return {
     handleIncrement: () => dispatch(increment(1)),
@@ -37,6 +48,4 @@ const mapDispatchToProps = (dispatch) => {
     handleDecrementByTwo: () => dispatch(decrement(2)),
   };
 };
-
 export default connect(null, mapDispatchToProps)(Counter);
-
