bb9e02578f3801d6b2c6dc7262af91674bb47477
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-18 17:53:06 +0530

    Space Added Between Buttons

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,58 +1,48 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Button, Paper } from '@mui/material';
-
 import { decrement, decrementByTwo, increment, incrementByTwo } from '../../store/reducers/Reducers';
 
-
 const Counter = () => {
+    const dispatch = useDispatch();
 
-const dispatch = useDispatch();
+    const handleIncrement = () => {
+        dispatch(increment());
+    };
 
-const handleIncrement = () => {
-dispatch(increment());
-};
+    const handleDecrement = () => {
+        dispatch(decrement());
+    };
 
-const handleDecrement = () => {
-dispatch(decrement());
-};
+    const handleIncrementByTwo = () => {
+        dispatch(incrementByTwo());
+    };
 
-const handleIncrementByTwo = () => {
-dispatch(incrementByTwo());
-};
+    const handleDecrementByTwo = () => {
+        dispatch(decrementByTwo());
+    };
 
-const handleDecrementByTwo = () => {
-dispatch(decrementByTwo());
-};
+    return (
+        <Paper>
+            <>
+                <Button variant="contained" color="primary" onClick={handleIncrement}>
+                    Add + 1
+                </Button>
+                <Button variant="contained" color="secondary" onClick={handleDecrement} style={{ marginRight: '1rem', marginLeft: '1rem' }}>
+                    Sub - 1
+                </Button>
+            </>
 
-return (
-<Paper>
-<>
-<Button variant="contained" color="primary" onClick={handleIncrement}>
-Add + 1
-</Button>
-<Button variant="contained" color="secondary" onClick={handleDecrement}>
-Sub - 1
-</Button>
-</>
-<>
-<Button
-variant="contained"
-color="primary"
-onClick={handleIncrementByTwo}
->
-Add + 2
-</Button>
-<Button
-variant="contained"
-color="secondary"
-onClick={handleDecrementByTwo}
->
-Sub - 2
-</Button>
-</>
-</Paper>
-);
+            <>
+                <Button variant="contained" color="primary" onClick={handleIncrementByTwo} style={{ marginLeft: '1rem' }}>
+                    Add + 2
+                </Button>
+                <Button variant="contained" color="secondary" onClick={handleDecrementByTwo} style={{ marginRight: '1rem', marginLeft: '1rem' }}>
+                    Sub - 2
+                </Button>
+            </>
+        </Paper>
+    );
 };
 
 export default Counter;
