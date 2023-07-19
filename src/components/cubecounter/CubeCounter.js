1224cdb1386c0c14a13e6616060c5639597be5ab
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-19 14:57:13 +0530

    CubeCounter Updated

--- src/components/cubecounter/CubeCounter.js
+++ src/components/cubecounter/CubeCounter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Cube from '../cube/Cube';
 import Counter from '../counter/Counter';
-import { increment, decrement } from "../../store/actions/Actions";
+import { increment,decrement } from '../../store/reducers/Reducers';
 
 const CubeCounter = ({ count, increment, decrement }) => {
   return (
