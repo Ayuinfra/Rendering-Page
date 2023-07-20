b1b555e80cc4e804307093a30b403a82a166958d
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-20 14:19:23 +0530

    Code Updated

--- src/components/productgrid/ProductGrid.js
+++ src/components/productgrid/ProductGrid.js
@@ -21,10 +21,13 @@ const ProductGrid = () => {
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return <>
+    Error: {error}
+    </>;
   }
 
   return (
+
     <Grid container spacing={2}>
       {products.map((product) => (
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
