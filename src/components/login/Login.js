b5849a3100ed32634736be71ecd588ef3507b1b5
Ashmit Tyagi <atyagi@infrablok.com>
2023-07-21 15:54:46 +0530

    Code Updated 1.Login and App.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,80 +1,74 @@
-import React, { useState } from 'react';
-import { Button, TextField, Typography, Container, Card, CardContent } from '@mui/material';
-import { useNavigate } from 'react-router';
+import React, { useState } from "react";
+import { Button, TextField, Typography, Container, Card, CardContent } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const Login = ({
-  handleLogin
-}) => {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+const Login = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-  const onMoveSignupHandler = () => {
-    navigate('../signup');
-  };
+    const onMoveSignupHandler = () => {
+        navigate("../signup");
+    };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    setError(''); 
-  };
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        setError("");
+    };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    setError('');
-  };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+        setError("");
+    };
 
-  const validateLoginFields = () => {
-    if (email.trim() === '' || password.trim() === '') {
-      setError('Please enter email and password');
-      return false;
-    }
-    return true;
-  };
+    const validateLoginFields = () => {
+        if (email.trim() === "" || password.trim() === "") {
+            setError("Please enter email and password");
+            return false;
+        }
+        return true;
+    };
 
-  const handleLoginClick = () => {
-    if (validateLoginFields()) {
-      const success = handleLogin(email, password);
-      if (success) 
-        navigate('/home');
-       else 
-        setError('Invalid email or password');
-    }
-  };
+    const handleLoginClick = () => {
+        if (validateLoginFields()) {
 
-  return (
-    <Container maxWidth="sm">
-      <Card>
-        <CardContent>
-          <Typography variant="h5" component="div" align="center">
-            Login
-          </Typography>
-          {error && <Typography color="error">{error}</Typography>}
-          <TextField
-            fullWidth
-            label="Email"
-            value={email}
-            onChange={handleEmailChange}
-            margin="normal"
-          />
-          <TextField
-            fullWidth
-            label="Password"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            margin="normal"
-          />
-          <Button variant="contained" onClick={handleLoginClick}>
-            Login
-          </Button>
-          <Button variant="contained" sx={{ marginLeft: '10px' }} onClick={onMoveSignupHandler}>
-            Signup
-          </Button>
-        </CardContent>
-      </Card>
-    </Container>
-  );
+            if (email === "example@example.com" && password === "password") {
+                setError("");
+                navigate("/ProductCard");
+            } else {
+                setError("Invalid email or password");
+            }
+        }
+    };
+
+    return (
+        <Container maxWidth="sm">
+            <Card>
+                <CardContent>
+                    <Typography variant="h5" component="div" align="center">
+                        Login
+                    </Typography>
+                    {error && <Typography color="error">{error}</Typography>}
+                    <TextField fullWidth label="Email" value={email} onChange={handleEmailChange} margin="normal" />
+                    <TextField
+                        fullWidth
+                        label="Password"
+                        type="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        margin="normal"
+                    />
+                    <Button variant="contained" onClick={handleLoginClick}>
+                        Login
+                    </Button>
+                    <Button variant="contained" sx={{ marginLeft: "10px" }} onClick={onMoveSignupHandler}>
+                        Signup
+                    </Button>
+                </CardContent>
+            </Card>
+        </Container>
+    );
 };
 
 export default Login;
